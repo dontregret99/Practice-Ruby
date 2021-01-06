04af97deeb2b8189810cbca2b00901f0d2b8f954
dontregret99 <ralph.le.goldenowl@gmail.com>
2021-01-06 12:20:57 +0700

    Update Exercise 4

--- 4.rb
+++ 4.rb
@@ -1,8 +1,13 @@
 def evaluate(n)
-    puts "You can't enter a negative number" if n < 0
-    puts "#{n} is between 0 and 50" if n.between?(0,50)
-    puts "#{n} is between 50 and 100" if n.between?(50,100)
-    puts "#{n} is above 100" if n > 100
+    if n < 0
+        puts "You can't enter a negative number"
+    elsif n.between?(0,50)
+        puts "#{n} is between 0 and 50"
+    elsif n.between?(50,100)
+        puts "#{n} is between 50 and 100"
+    else
+        puts "#{n} is above 100"
+    end
 end
 
 n = gets.to_i

e6922a804d7023fd4611813ecc142bc316778c22
dontregret99 <ralph.le.goldenowl@gmail.com>
2021-01-06 12:25:45 +0700

    Update 5.rb: check lenght of input string

--- 5.rb
+++ 5.rb
@@ -1,7 +1,12 @@
 def remove_first_last_two_char(str)
     result = ""
-    pos = 0
     len = str.length
+
+    if len <= 4
+        return result
+        
+    # use pos + .each as .each_with_index
+    pos = 0
     str.split('').each do 
         |c|
         if pos != 0 && pos != 1 && pos != len-1 && pos != len-2

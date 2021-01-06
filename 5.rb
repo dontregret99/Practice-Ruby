80cf5bcf5c93de9745622e0896d582abf6e81091
dontregret99 <ralph.le.goldenowl@gmail.com>
2021-01-06 12:12:23 +0700

    Update exercise 5

--- 5.rb
+++ 5.rb
@@ -1,6 +1,14 @@
 def remove_first_last_two_char(str)
     result = ""
-    str.each do 
+    pos = 0
+    len = str.length
+    str.split('').each do 
         |c|
+        if pos != 0 && pos != 1 && pos != len-1 && pos != len-2
+            result += c
+        end
+        pos += 1
     end
+    return result
 end
+puts remove_first_last_two_char('Javascript')
